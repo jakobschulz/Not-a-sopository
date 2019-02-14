5afdeb32b65d3b56831c9d316ce71edb8ba3dfba
jakobschulz <47540308+jakobschulz@users.noreply.github.com>
2019-02-15 12:08:43 +1300

    Add files via upload

---
+++ untitled-1.py
@@ -0,0 +1,11 @@
+# Not-a-sopository(Sign up)
+#build log for sign up web page
+#ver1.1 - Created test data for ticket variable
+#ver1.2 added server functionality to python
+
+
+#test data
+tickets =[Ticket("Jakob Schulz", "jakobschulz@email.com","20.02.2003",False),
+          Ticket("John Connner","Terminator@email.com","12.12.2012",False),
+          Ticket("John Cena","Invisible@email.com","01.01.1991",False),
+          Ticket("John Wick","Wicket@email.com","06.09.1969",False)]

fb75f8c01918a71d819cc38d935c2de6a495be4b
jakobschulz <47540308+jakobschulz@users.noreply.github.com>
2019-02-27 15:07:54 +1300

    Add files via upload

--- Website.py
+++ Website.py
@@ -22,7 +22,7 @@ class Ticket: #class is an object which holds information
 
 
 #test data
-tickets =[Ticket("Jakob Schulz", "jakobschulz@email.com","20.02.2003",False),
+tickets =[Ticket("Jakob Schulz", "jakobschulz@email.com","20.02.2003",True),
           Ticket("John Connner","Terminator@email.com","12.12.2012",False),
           Ticket("John Cena","Invisible@email.com","01.01.1991",False),
           Ticket("John Wick","HisDog@email.com","06.09.1969",False)]
@@ -44,7 +44,23 @@ def check_in():
     data = dict (ticket_list = tickets)
     return data
 
+@route('/check-in-success/<ticket_id>')
+@view('check-in-success')
+def check_in_success(ticket_id):
+    #need this function to attach the decorators above.
+    ticket_id = int(ticket_id)
+    found_ticket = None
+    for ticket in tickets:
+        if ticket.id == ticket_id:
+            found_ticket = ticket
+    data = dict (ticket = found_ticket)
+    found_ticket.check_in = True
+    return data
 
+@route('/sell-ticket')
+@view('sell-ticket')
+def sell_ticket():
+    pass
 
 
 
